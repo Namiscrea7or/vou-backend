284f340b12b2d79412099f1cb696de859868f5f8
ViTinh TTC <pphnam21@clc.fitus.edu.vn>
2024-09-02 16:48:31 +0700

    chore: add login to schema

--- pkg/core/schema.go
+++ pkg/core/schema.go
@@ -63,6 +63,7 @@ func InitSchema() graphql.Schema {
 		Name: "RootMutation",
 		Fields: graphql.Fields{
 			"registerAccount":                usersMutation.RegisterAccount,
+			"login":                          usersMutation.Login,
 			"createVoucher":                  vouchersMutation.CreateVoucher,
 			"editVoucher":                    vouchersMutation.EditVoucher,
 			"deleteVoucher":                  vouchersMutation.DeleteVoucher,
