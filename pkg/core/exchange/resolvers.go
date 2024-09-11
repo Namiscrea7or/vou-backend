2ffefd3f95a10ed9caa166c43e941e20f732a628
ViTinh TTC <pphnam21@clc.fitus.edu.vn>
2024-09-11 13:07:05 +0700

    chore: change some error line

--- pkg/core/exchange/resolvers.go
+++ pkg/core/exchange/resolvers.go
@@ -151,7 +151,7 @@ func (r *ExchangesResolver) GetAllExchangesByGameSessionID(params graphql.Resolv
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	cursor, err := db.GetExchangeCollection().Find(ctx, bson.M{"game_session_id": sessionID.Hex()})
+	cursor, err := db.GetExchangeCollection().Find(ctx, bson.M{"gameSession_id": sessionID.Hex()})
 	if err != nil {
 		log.Printf("failed to fetch exchanges: %v\n", err)
 		return nil, err
